28ace9c31332059a98fe163dbf6a33444d1edeb7
Jay Ma <doudoujay@qq.com>
2018-09-18 16:11:38 -0400

    adopt screen

--- README.md
+++ README.md
@@ -13,3 +13,16 @@ npm install
 ```
 npm run serve
 ```
+
+
+a. The cards should be laid out in a grid
+
+b. Click to turn over any two cards.
+
+c. If the two cards match, keep them in a revealed state.
+
+d. If they don't match, turn them back over.
+
+e. (If in multiplayer mode) Watch and remember during the other player's turn.
+
+f. The game is over when all the cards have been matched
--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,15 +1,33 @@
 class HomeController {
-  constructor($scope,GameService,$rootScope) {
+  constructor($scope, GameService, $rootScope, $window) {
     this.name = 'home';
     this.$scope = $scope;
     this.GameService = GameService;
-    GameService.reset(6,6);
+    GameService.reset(6, 6);
     // this.$scope.grid = GameService.getGrid();
 
-    this.$scope.flip = (i, j) => GameService.flip(i,j);
+    this.$scope.flip = (i, j) => GameService.flip(i, j);
+    $scope.resize = () => {
+      console.log($window.innerWidth, $window.innerHeight);
+      $scope.cardHeight = (($window.innerHeight - 112) / GameService.height) - 18;
+      $scope.cardStyle = {
+        'height': $scope.cardHeight + 'px',
+        'width': 'auto'
+      };
+    };
+
+    $scope.resize();
 
 
+    var appWindow = angular.element($window);
+
+    appWindow.bind('resize', () => {
+      $scope.$apply(() => {
+        $scope.resize();
+      })
+    });
   }
 }
-HomeController.$inject = ['$scope','GameService', '$rootScope'];
+
+HomeController.$inject = ['$scope', 'GameService', '$rootScope', '$window'];
 export default HomeController;
